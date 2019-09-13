d53c13405ab717e45f7560759bde166ed4ebb59f
Aleksei Sanin <aleksei.sanin@epfl.ch>
2019-09-13 17:53:57 +0200

    example if failing model from ModelDB

--- examples/export_neuroml2.py
+++ examples/export_neuroml2.py
@@ -1,8 +1,9 @@
 from pyneuroml.neuron import export_to_neuroml2
 
-export_to_neuroml2("test.hoc", "test.morphonly.cell.nml", includeBiophysicalProperties=False)
+# export_to_neuroml2("test.hoc", "test.morphonly.cell.nml", includeBiophysicalProperties=False)
+# export_to_neuroml2("test.hoc", "test.biophys.cell.nml", includeBiophysicalProperties=True)
 
-export_to_neuroml2("test.hoc", "test.biophys.cell.nml", includeBiophysicalProperties=True)
+export_to_neuroml2("Cell_Scnn1a.hoc", "Cell_Scnn1a.cell.nml", includeBiophysicalProperties=True)
 
 
 
